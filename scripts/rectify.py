1ba3e179d0a2537414ad12b56ea498f51e29685a
NicoLingg <n.lingg20@imperial.ac.uk>
2022-02-02 16:37:35 +0000

    Imperial PRL T265 adaptation

--- scripts/rectify.py
+++ scripts/rectify.py
@@ -30,13 +30,14 @@ def load_yaml(filepath):
 
 class Rectification(object):
     def __init__(self):
-        self.left_image_pub = rospy.Publisher("/camera/fisheye1/image_rect", Image, queue_size=1)
-        self.right_image_pub = rospy.Publisher("/camera/fisheye2/image_rect", Image, queue_size=1)
-        self.camera_cb
-        left_cam_sub = message_filters.Subscriber("/camera/fisheye1/image_raw", Image)
-        right_cam_sub = message_filters.Subscriber("/camera/fisheye2/image_raw", Image)
-        ts = message_filters.TimeSynchronizer([left_cam_sub, right_cam_sub], 10)
-        ts.registerCallback(self.camera_cb)
+
+        camera_name = rospy.get_param("~t265_cam_namespace", default="/t265")
+
+        self.left_image_pub = rospy.Publisher(camera_name + "/fisheye1/image_rect", Image, queue_size=1)
+        self.right_image_pub = rospy.Publisher(camera_name + "/fisheye2/image_rect", Image, queue_size=1)
+        
+        left_cam_sub = message_filters.Subscriber(camera_name + "/fisheye1/image_raw", Image)
+        right_cam_sub = message_filters.Subscriber(camera_name + "/fisheye2/image_raw", Image)
         # Create distortion maps for left and right
         image_size = (int(camera_params["width"]), int(camera_params["height"]))
         m1type = cv2.CV_16SC2
@@ -55,16 +56,20 @@ class Rectification(object):
             camera_params["P2"], 
             image_size, 
             m1type)
+        
         self.undistort_rectify = { "left": undistortion_map_1, "right": undistortion_map_2 }
 
+        ts = message_filters.TimeSynchronizer([left_cam_sub, right_cam_sub], 10)
+        ts.registerCallback(self.camera_cb)
+
     def cv_to_ros(self, image, message):
-        image_message = bridge.cv2_to_imgmsg(image, encoding="passthrough")
+        image_message = bridge.cv2_to_imgmsg(image, encoding="rgb8")
         image_message.header = message.header
         return image_message
 
     def camera_cb(self, img_left, img_right):
-        cv_left = bridge.imgmsg_to_cv2(img_left, desired_encoding='passthrough')
-        cv_right = bridge.imgmsg_to_cv2(img_right, desired_encoding='passthrough')
+        cv_left = bridge.imgmsg_to_cv2(img_left, desired_encoding='rgb8')
+        cv_right = bridge.imgmsg_to_cv2(img_right, desired_encoding='rgb8')
         cv_left_undistorted = cv2.remap(src=cv_left, map1=self.undistort_rectify["left"][0], map2=self.undistort_rectify["left"][1], interpolation=cv2.INTER_LINEAR)
         cv_right_undistorted = cv2.remap(src=cv_right, map1=self.undistort_rectify["right"][0], map2=self.undistort_rectify["right"][1], interpolation=cv2.INTER_LINEAR)
         self.left_image_pub.publish(self.cv_to_ros(cv_left_undistorted, img_left))
@@ -78,7 +83,7 @@ def main():
 
 if __name__ == '__main__':
     try:
-        load_yaml("/home/nvidia/catkin_ws/src/fisheye-rectifier/params/t265_params.yml")
+        load_yaml("/root/ros_ws/src/fisheye-rectifier/params/t265_params.yml")
         main()
     except rospy.ROSInterruptException:
         pass
